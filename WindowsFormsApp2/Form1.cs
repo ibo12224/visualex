d523e617d9f9f33c5db1cf13824a1c33f0cd3190
ibo12224 <113340577+ibo12224@users.noreply.github.com>
2022-12-03 11:43:05 +0900

    1234324

---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        private void UpdateLabel(string s, bool b)
+        {
+            if (s == "적용")
+            {
+                if (b)
+                {
+                    fontDialog1.ShowApply = true;
+
+                }
+                else
+                {
+                    fontDialog1.ShowApply = false;
+                }
+            }
+            if (s == "색상")
+            {
+                if (b)
+                {
+                    fontDialog1.ShowColor = true;
+                }
+                else
+                {
+                    fontDialog1.ShowColor = false;
+                }
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void fontDialog1_Apply(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            UpdateLabel(checkBox1.Text, checkBox1.Checked);
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            UpdateLabel(checkBox2.Text, checkBox2.Checked);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            fontDialog1.ShowDialog();
+
+        }
+    }
+}
