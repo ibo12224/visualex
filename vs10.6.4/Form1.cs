94ee317767e91ee188ade7a2d5f4ae79b88f8304
ibo12224 <113340577+ibo12224@users.noreply.github.com>
2022-12-04 10:01:33 +0900

    1234

--- vs10.6.4/Form1.cs
+++ vs10.6.4/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +16,32 @@ namespace vs10._6._4
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            System.Text.StringBuilder messageBoxCS = new System.Text.StringBuilder();
+
+
+            if (e.Control&&e.KeyCode==Keys.F1) { messageBoxCS.AppendFormat("{0}+Control", e.KeyCode);
+                MessageBox.Show(messageBoxCS.ToString(), "누른 키");
+                
+            }
+            else if (e.Shift && e.KeyCode == Keys.F1) { messageBoxCS.AppendFormat("{0}+Shift", e.KeyCode);
+                MessageBox.Show(messageBoxCS.ToString(), "누른 키");
+                
+            }
+            else if(e.Alt && e.KeyCode == Keys.F1) { messageBoxCS.AppendFormat("{0}+Alt", e.KeyCode);
+                MessageBox.Show(messageBoxCS.ToString(), "누른 키");
+                
+            }
+            
+           
+            
+            
+
+
+           
         }
     }
 }
---
+++ vs10.6.4/Form2.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace vs10._6._4
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(250, 79);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(34, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 45);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 131);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
