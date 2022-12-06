49cadbc98b30e1fb600044a0f57aa0546791fb97
ibo12224 <113340577+ibo12224@users.noreply.github.com>
2022-12-06 21:58:05 +0900

    ss

--- vs10.6.7/Form1.cs
+++ vs10.6.7/Form1.cs
@@ -20,7 +20,7 @@ namespace vs10._6._7
         private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
         {
             char c = e.KeyChar;
-            if (c == '1') return;///////////234567890124456771ssiiiissssssssssssssssssssssssssssssssssssssssssssssss
+            if (c == '1') return;///////////234567890124456771ssiiiissssssssssssssssssssssssssssssssssssssssssssssssss
             if (c == '2') return;
             if (c == '3') return;
             if (c == '4') return;
